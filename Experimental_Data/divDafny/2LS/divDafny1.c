78fc25d4036381f6a6c89597df360e781fe157b3
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-11-29 22:50:10 +0800

    Add files via upload

---
+++ Experimental_Data/divDafny/2LS/divDafny1.c
@@ -0,0 +1,19 @@
+void main()
+{
+			
+		
+	int r,q;
+        int X;
+        int Y;
+	r = X;
+    	q = 0;
+        assume(X>=0 && Y>0);
+        
+    	while(r >= Y) {
+      		r = r - Y;
+      		q = q + 1;
+    	}
+	assert(X==q*Y+r);
+	
+	
+}

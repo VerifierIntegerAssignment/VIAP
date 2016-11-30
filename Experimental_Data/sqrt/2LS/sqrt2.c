49534d04574dd4cdff4dcdc00d4bf2a9f28ebdde
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-11-30 12:05:22 +0800

    Add files via upload

---
+++ Experimental_Data/sqrt/2LS/sqrt2.c
@@ -0,0 +1,24 @@
+void main()
+{
+
+	
+
+	int a,su,t;
+        int X;
+
+    	a=0;
+    	su=1;
+    	t=1;
+
+        assume(X>=0);
+    	while ( su <= X )
+        {
+        a=a+1;
+        t=t+2;
+        su=su+t;
+        }
+
+     assert(a*a<=X);
+	
+
+}

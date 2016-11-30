b4728061615708767b0162f4a84028a991d1d098
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-11-30 12:13:43 +0800

    Add files via upload

---
+++ Experimental_Data/sumOfEven/2LS/sumOfEven.c
@@ -0,0 +1,22 @@
+void main()
+{
+
+	
+
+		
+		int sum,i,X;
+                int X;
+		sum=0;
+		i=0;
+                assume(X>=0);
+		while(i<X)
+		{
+			i=i+1;
+			sum=sum+i*2;
+			
+		}
+                assert(sum == X*(X+1));
+
+	
+
+}

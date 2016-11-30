d16a74514760d27f86fc85101fa7b38d1e5b3bb5
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-11-30 12:14:41 +0800

    Add files via upload

---
+++ Experimental_Data/sumOfOdd/2LS/sumOfOdd.c
@@ -0,0 +1,21 @@
+void main()
+{
+
+	
+
+		int sum,i;
+		sum=0;
+		i=0;
+                int X;
+                assume(X>=0);
+		while(i<X)
+		{
+
+			sum=sum+2*i+1;
+			i=i+1;
+
+		}
+	       assert(sum == X*X);
+	
+
+}

73466694bdda6a57eac47be53ba1cb51d8a65266
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-11-30 12:37:15 +0800

    Add files via upload

---
+++ Experimental_Data/divKaldewaij/automizer/divKaldewaij1.c
@@ -0,0 +1,46 @@
+extern void __VERIFIER_error(void);
+extern void __VERIFIER_assume(int);
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+      ERROR: __VERIFIER_error();
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
+void main()
+{
+			
+		
+	    int q;
+	    int r;
+	    int b;    
+	    int A;
+            int B;
+
+	    q=0;
+	    r=A;
+	    b=B;
+        __VERIFIER_assume(A>=0 && B>0);
+    
+   	 while(r>=b)
+       	 {
+       		 b=2*b;
+         }
+
+    
+    	while(b!=B)
+        {
+        q=2*q;
+        b=b/2;
+
+        if (r>=b) 
+            {
+            q=q+1;
+            r=r-b;
+            }
+
+        }
+	
+	__VERIFIER_assert(A==q*B+r);
+	
+}

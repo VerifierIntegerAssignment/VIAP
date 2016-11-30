3ff46bf2c16d34ebc57dabca912f8a223b7bb8a0
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-11-30 11:50:52 +0800

    Add files via upload

---
+++ Experimental_Data/potSumm4/2LS/potSumm4.c
@@ -0,0 +1,20 @@
+void main()
+{
+
+		
+		int sum,i;
+                int X;
+		sum=0;
+		i=0;
+                assume(X>=0);
+		while(i<X)
+		{
+			i=i+1;
+			sum=sum+i*i*i*i;
+			
+		}
+                assert(sum == (6*X*X*X*X*X+15*X*X*X*X+10*X*X*X-X)/30);
+		
+
+
+}

5ffd4e0543b75853365065502e0c92953b4c9d68
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-11-30 12:13:15 +0800

    Add files via upload

---
+++ Experimental_Data/square/2LS/square.c
@@ -0,0 +1,20 @@
+void main()
+{
+
+	
+		int s,i;
+                int M;
+		s=0;
+		
+		i=0;
+		assume(M>=0)
+		while(i<M)
+		{
+			s=s+M;
+			i=i+1;
+			
+		}
+
+                assert(s == M*M);
+
+}

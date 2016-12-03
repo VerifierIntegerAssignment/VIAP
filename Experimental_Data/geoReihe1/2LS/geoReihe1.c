31b7e5934ee44993497de8abec4c50f719c675bc
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-12-03 20:32:38 +0800

    Add files via upload

--- Experimental_Data/geoReihe1/2LS/geoReihe1.c
+++ Experimental_Data/geoReihe1/2LS/geoReihe1.c
@@ -1,3 +1,12 @@
+extern void __VERIFIER_error(void);
+extern void __VERIFIER_assume(int);
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+      ERROR: __VERIFIER_error();
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
 void main()
 {
 
@@ -11,7 +20,7 @@ void main()
   		l = Z;
   		k = 1;
     
-    assume(Z>1 && K>0);
+    __VERIFIER_assume(Z>1 && K>0);
     
   		while (k < K){
     			k = k + 1;
@@ -20,12 +29,12 @@ void main()
   		}
 
   		m = m *(Z -1);
-    assert(m == ((power(Z,K)-1)/(Z-1))*(Z-1));
+    __VERIFIER_assert(m == ((power(Z,K)-1)/(Z-1))*(Z-1));
 }
 
 int power(int a,int b)
 {
-    assume(a>=0 && b>=0);
+    __VERIFIER_assume(a>=0 && b>=0);
     if (a == 0 && b>0)
     { return 0;
     }

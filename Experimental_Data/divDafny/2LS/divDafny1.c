aca55f2c55c94d6dfdaa783cb83232a9ab68c319
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-12-03 20:30:02 +0800

    Add files via upload

--- Experimental_Data/divDafny/2LS/divDafny1.c
+++ Experimental_Data/divDafny/2LS/divDafny1.c
@@ -1,3 +1,12 @@
+extern void __VERIFIER_error(void);
+extern void __VERIFIER_assume(int);
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+      ERROR: __VERIFIER_error();
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
 void main()
 {
 			
@@ -7,13 +16,13 @@ void main()
         int Y;
 	r = X;
     	q = 0;
-        assume(X>=0 && Y>0);
+        __VERIFIER_assume(X>=0 && Y>0);
         
     	while(r >= Y) {
       		r = r - Y;
       		q = q + 1;
     	}
-	assert(X==q*Y+r);
+	__VERIFIER_assert(X==q*Y+r);
 	
 	
 }

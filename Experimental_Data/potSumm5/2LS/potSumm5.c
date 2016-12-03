8da136a29f0e02782bd79b30060919b329876bad
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-12-03 20:38:54 +0800

    Add files via upload

--- Experimental_Data/potSumm5/2LS/potSumm5.c
+++ Experimental_Data/potSumm5/2LS/potSumm5.c
@@ -1,3 +1,12 @@
+extern void __VERIFIER_error(void);
+extern void __VERIFIER_assume(int);
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+      ERROR: __VERIFIER_error();
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
 void main()
 {
 
@@ -6,14 +15,14 @@ void main()
                 int X;
 		sum=0;
 		i=0;
-                assume(X>=0);
+                __VERIFIER_assume(X>=0);
 		while(i<X)
 		{
 			i=i+1;
 			sum=sum+i*i*i*i*i;
 			
 		}
-		assert(sum == (2*X*X*X*X*X*X+6*X*X*X*X*X+5*X*X*X*X-X*X)/12);
+		__VERIFIER_assert(sum == (2*X*X*X*X*X*X+6*X*X*X*X*X+5*X*X*X*X-X*X)/12);
 
 
 }

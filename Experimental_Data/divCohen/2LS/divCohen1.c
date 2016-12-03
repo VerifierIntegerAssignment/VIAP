f3acf05e0548625736e2e6e072354e4df63b177f
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-12-03 20:29:23 +0800

    Add files via upload

--- Experimental_Data/divCohen/2LS/divCohen1.c
+++ Experimental_Data/divCohen/2LS/divCohen1.c
@@ -1,3 +1,12 @@
+extern void __VERIFIER_error(void);
+extern void __VERIFIER_assume(int);
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+      ERROR: __VERIFIER_error();
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
 void main()
 {
 		 int q;
@@ -10,7 +19,7 @@ void main()
 		q=0;
 		r=X;	
 
-		assume(X >= 0 && Y > 0);
+		__VERIFIER_assume(X >= 0 && Y > 0);
 		while(r >= Y)
 		{
 			A=1;
@@ -25,5 +34,5 @@ void main()
 		q=q+A;
 			
 		}
-                assert(X==q*Y+r);
+                __VERIFIER_assert(X==q*Y+r);
 }

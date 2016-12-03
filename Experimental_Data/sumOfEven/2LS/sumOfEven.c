3c0c19ca1e1f494310c60feea44359c4cc14c2eb
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-12-03 20:46:33 +0800

    Add files via upload

--- Experimental_Data/sumOfEven/2LS/sumOfEven.c
+++ Experimental_Data/sumOfEven/2LS/sumOfEven.c
@@ -1,3 +1,13 @@
+extern void __VERIFIER_error(void);
+extern void __VERIFIER_assume(int);
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+      ERROR: __VERIFIER_error();
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
+
 void main()
 {
 
@@ -8,14 +18,14 @@ void main()
                 int X;
 		sum=0;
 		i=0;
-                assume(X>=0);
+                __VERIFIER_assume(X>=0);
 		while(i<X)
 		{
 			i=i+1;
 			sum=sum+i*2;
 			
 		}
-                assert(sum == X*(X+1));
+                __VERIFIER_assert(sum == X*(X+1));
 
 	
 
